f0a510ba88979d8a51886e1369f71842f1874617
Ricardo <rscimc@gmail.com>
2023-06-13 23:10:50 -0300

    Importação de sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_tutor (nome, email, senha) VALUES ('Ricardo', 'ricardo@gmail.com', 'ricardo1');
+INSERT INTO tb_tutor (nome, email, senha) VALUES ('Deborah', 'deborah@gmail.com', 'deborah1');
+INSERT INTO tb_tutor (nome, email, senha) VALUES ('Letícia', 'leticia@gmail.com', 'leticia1');
+INSERT INTO tb_tutor (nome, email, senha) VALUES ('Isabella', 'isabella@gmail.com', 'isabella1');
